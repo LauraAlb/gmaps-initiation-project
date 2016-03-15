d54c553e68b5bb18b9cae181d6f10c248ccb2159
LauraAlb <LauraAlb@3199125.no-reply.drupal.org>
2016-03-15 16:03:24 +0100

    fix

--- src/app/components/snwMap/elements/exerciseOne/exerciseOneCtrl.js
+++ src/app/components/snwMap/elements/exerciseOne/exerciseOneCtrl.js
@@ -2,10 +2,10 @@
   'use strict';
   var exerciseOneCtrl = function($scope, gmapsService) {
     function init() {
-      var geocoder, infowindow, formatAdress;
+      var geocoder, formatAdress;
       formatAdress = 'formatted_address';
       geocoder = new google.maps.Geocoder();
-      infowindow = new google.maps.InfoWindow();
+      $scope.infowindow = new google.maps.InfoWindow();
       $scope.marker = gmapsService.createMarker($scope.map, $scope.map.getCenter());
       $scope.eventClick = google.maps.event.addListener($scope.map, 'click', function(event) {
         geocoder.geocode({
@@ -22,7 +22,7 @@
             if (results.length && results[0][formatAdress]) {
               contentString = results[0][formatAdress];
             }
-            gmapsService.updateInfoWindow($scope.map, $scope.marker, infowindow, contentString);
+            gmapsService.updateInfoWindow($scope.map, $scope.marker, $scope.infowindow, contentString);
           } else {
             window.alert('Geocoder failed due to: ' + status);
           }
@@ -30,9 +30,14 @@
       });
     }
 
+    function deleteMarker() {
+      $scope.marker.setMap(null);
+      $scope.infowindow.close();
+      google.maps.event.removeListener($scope.eventClick);
+    }
     init();
     $scope.$on('$destroy', function() {
-      google.maps.event.removeListener($scope.eventClick);
+      deleteMarker();
     });
   };
 
